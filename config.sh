9125afdc6e825841d653c61e4206875e687c246c
Khadim <khadimfall0602@gmail.com>
2022-11-30 22:18:39 +0100

    updated repos

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 export APPS_FOLDER="${SCRIPT_DIR}/apps"
 export APPS=(
   "next pawn.town.app git@github.com:mono424/pawn.town.app.git .env.app.local env.local"
-  # "go pawn.town.live.api git@github.com:mono424/pawn.town.live.api.git api.local.env env.dev" // not sure if needed
+  # "go live.pawn.town.api git@github.com:mono424/live.pawn.town.api.git api.local.env env.dev" // not sure if needed
   "go pawn.town.api git@github.com:mono424/pawn.town.api.git .env.api.local env.dev"
 )
 
