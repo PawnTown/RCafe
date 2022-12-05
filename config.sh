180b810426c14fcd85fe8b107815561d1679e980
Khadim Fall <khadimfall0602@gmail.com>
2022-12-05 23:11:07 +0100

    Fix env names

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 export APPS_FOLDER="${SCRIPT_DIR}/apps"
 export APPS=(
-  "next pawn.town.app git@github.com:mono424/pawn.town.app.git .env.app.local env.local"
+  "next pawn.town.app git@github.com:mono424/pawn.town.app.git .env.app.local .env.local"
   # "go live.pawn.town.api git@github.com:mono424/live.pawn.town.api.git api.local.env env.dev" // not sure if needed
-  "go pawn.town.api git@github.com:mono424/pawn.town.api.git .env.api.local env.dev"
+  "go pawn.town.api git@github.com:mono424/pawn.town.api.git .env.api.local .env.dev"
 )
 
 echo "Config loaded."
